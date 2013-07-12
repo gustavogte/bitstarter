34709a4c5b5f67fe9ee73cb68ee4c4b33487422b
gustavo <gustavogte@gmail.com>
2013-07-12 19:30:01 +0000

    read index file

--- web.js
+++ web.js
@@ -1,9 +1,22 @@
 var express = require('express');
 
+// importar modulo para leer y escribir archivos 
+var fs = require('fs');
+
+///// leer archivo de index.html
+
+var leerArchivo = function(archivo) {
+    fs.readFile(archivo, function (err, data){
+	if (err) throw err;
+	console.log(data.toString());
+    });
+};
+////////
+ 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(leerArchivo('/Users/gustavogte/Google Drive/Actual_Hoy/StartUp/aws1/bitstarter/index.html'));
 });
 
 var port = process.env.PORT || 5000;
