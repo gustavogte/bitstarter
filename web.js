76c4988aaa80d3c6af699be3bff772a48452bc6f
gustavo <gustavogte@gmail.com>
2013-07-13 00:05:40 +0000

    otra correcion

--- web.js
+++ web.js
@@ -3,25 +3,16 @@ var express = require('express');
 // importar modulo para leer y escribir archivos 
 var fs = require('fs');
 
-///// leer archivo de index.html
-
-var leerArchivo = function(archivo) {
-    fs.readFile(archivo, function (err, data){
-	if (err) throw err;
-	data.toString();
-    });
-};
-////////
- 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
  //   response.send('Hola mundo 3');
   fs.readFile('./index.html', function (err, data){
-  if (err) throw err;
-  response.send(data.toString());
+      if (err) throw err;
+      var buffer = new Buffer(data);
+      response.send(buffer.toString('utf-8'));
   });
-
+    
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
