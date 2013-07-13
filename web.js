61ff0515055c88a585cfff64906e8ff14f670abc
gustavo <gustavogte@gmail.com>
2013-07-13 00:31:37 +0000

    1er intento nuevo

--- web.js
+++ web.js
@@ -4,7 +4,10 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-   response.send('Hola mundo 3');
+    fs.readFile('index.html', function (err, data){
+	if (err) throw err;
+	response.send(data.toString());
+    }
 });
     
 var port = process.env.PORT || 5000;
