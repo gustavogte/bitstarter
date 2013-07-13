da623ed20b8b8a6876167d37b4385d9fc4342f6f
gustavo <gustavogte@gmail.com>
2013-07-13 00:13:58 +0000

    otra correcion

--- web.js
+++ web.js
@@ -10,7 +10,8 @@ app.get('/', function(request, response) {
   fs.readFile('./index.html', function (err, data){
       if (err) throw err;
       var buffer = new Buffer(data);
-      response.send(buffer.toString('utf-8'));
+      var buf = buffer.toString();
+      response.send(buf);
   });
     
 var port = process.env.PORT || 5000;
