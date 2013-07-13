314f42749a4c76790a22dfe54ad94840c01febd4
gustavo <gustavogte@gmail.com>
2013-07-13 00:24:48 +0000

    comenzar de nuevo

--- web.js
+++ web.js
@@ -1,18 +1,11 @@
 var express = require('express');
-
-// importar modulo para leer y escribir archivos 
 var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
    response.send('Hola mundo 3');
-//  fs.readFile('./index.html', function (err, data){
-//      if (err) throw err;
-//      var buffer = new Buffer(data);
-//      var buf = buffer.toString();
-//      response.send(buf);
-//  });
+});
     
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
