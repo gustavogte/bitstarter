ce2a1a488800ba85c56ef6c252777c00e4c13d7e
gustavo <gustavogte@gmail.com>
2013-07-13 00:15:30 +0000

    otra correcion

--- web.js
+++ web.js
@@ -6,13 +6,13 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
- //   response.send('Hola mundo 3');
-  fs.readFile('./index.html', function (err, data){
-      if (err) throw err;
-      var buffer = new Buffer(data);
-      var buf = buffer.toString();
-      response.send(buf);
-  });
+   response.send('Hola mundo 3');
+//  fs.readFile('./index.html', function (err, data){
+//      if (err) throw err;
+//      var buffer = new Buffer(data);
+//      var buf = buffer.toString();
+//      response.send(buf);
+//  });
     
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
