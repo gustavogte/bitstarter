e58c253590e38d8aa97eaccd91b1f4a988f08f04
Ubuntu <ubuntu@ip-172-31-31-137.us-west-2.compute.internal>
2013-07-20 04:52:12 +0000

    desde awshost2

--- web.js
+++ web.js
@@ -3,17 +3,17 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    var fs = require ('fs'); // importar modulo para leer archivos
-    fs.readFile('index.html', 
-	function (err, data){
-	    if (err) throw err;
-	    response.send(data.toString());
-	}
+    var fs = require ('fs'); // importar modulo para leer archivos              
+    fs.readFile('index.html',
+        function (err, data){
+            if (err) throw err;
+            response.send(data.toString());
+        }
     );
-    // reponse.send('Hello World 2!')
+    // reponse.send('Hello World 2!') desde awshost 1                                        
 });
-    
-var port = process.env.PORT || 5000;
+  
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
